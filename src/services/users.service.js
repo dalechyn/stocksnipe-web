c027b514546c9dbb771046d4361d95dbf375dccf
h0tw4t3r <hotwater438@tutanota.com>
2020-03-05 20:36:05 +0200

    userService: fetchAuth

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,40 +1,78 @@
+import { useDispatch } from 'react-redux'
 import config from '../config/default'
 import { authHeader } from '../helpers'
+import {
+	getRefreshToken,
+	removeUserAndTokens,
+	setAccessToken,
+	setRefreshToken,
+	setUser
+} from '../utils'
+import { userActions } from '../actions'
 
 const makeHeaders = headers => ({
 	...headers,
 	...authHeader()
 })
 
-const reqHandler = (text, response) =>
-	response.ok ? text && JSON.parse(text) : Promise.reject(text || response.statusText)
+const resHandler = response => {
+	return response.ok
+		? Promise.resolve(response)
+				.then(response => response.text())
+				.then(text =>
+					text
+						? JSON.parse(text)
+						: Promise.reject(new Error('JSON Body response is empty'))
+				)
+		: Promise.reject(response)
+}
 
-const getTokenPair = refreshToken => {
+const fetchAuth = (
+	url,
+	reqOptions = {
+		method: 'GET'
+	}
+) => {
+	const { headers, ...restReqOptions } = reqOptions
+	const initRequest = fetch(url, { headers: makeHeaders(headers), ...restReqOptions })
+
+	return initRequest.catch(response => tokenHandler(response, initRequest))
+}
+
+const tokenHandler = (initPromise, response) => {
+	if (response.status === 401) {
+		const dispatch = useDispatch()
+		dispatch(userActions.getTokenPair)
+		return refreshTokenPair(getRefreshToken())
+			.then(initPromise)
+			.catch(response => response.statusText)
+	}
+	return response
+}
+
+const refreshTokenPair = refreshToken => {
 	const requestOptions = {
 		method: 'POST',
-		headers: makeHeaders({
+		headers: {
 			'Content-Type': 'application/json'
-		}),
+		},
 		body: JSON.stringify({ refreshToken })
 	}
 
-	return fetch(`${config.api.url}/auth/refresh`, requestOptions).then(response =>
-		response
-			.text()
-			.then(text => reqHandler(text, response))
-			.then((accessToken, refreshToken) => {
-				localStorage.setItem('accessToken', accessToken)
-				localStorage.setItem('refreshToken', refreshToken)
-			})
-	)
+	return fetch(`${config.api.url}/auth/refresh`, requestOptions)
+		.then(resHandler)
+		.then(({ accessToken, refreshToken }) => {
+			setAccessToken(accessToken)
+			setRefreshToken(refreshToken)
+		})
 }
 
 const register = (userEmail, userLogin, userPassword) => {
 	const requestOptions = {
 		method: 'POST',
-		headers: makeHeaders({
+		headers: {
 			'Content-Type': 'application/json'
-		}),
+		},
 		body: JSON.stringify({ email: userEmail, login: userLogin, password: userPassword })
 	}
 
@@ -44,34 +82,31 @@ const register = (userEmail, userLogin, userPassword) => {
 const login = (userLogin, userPassword) => {
 	const requestOptions = {
 		method: 'POST',
-		headers: makeHeaders({
+		headers: {
 			'Content-Type': 'application/json'
-		}),
+		},
 		body: JSON.stringify({ login: userLogin, password: userPassword })
 	}
 
 	return fetch(`${config.api.url}/auth/login`, requestOptions)
-		.then(response => response.text().then(text => reqHandler(text, response)))
+		.then(resHandler)
 		.then(user => {
+			console.log(user)
 			const { refreshToken, accessToken, ...rest } = user
 
-			localStorage.setItem('accessToken', accessToken)
-			localStorage.setItem('refreshToken', refreshToken)
-			localStorage.setItem('user', JSON.stringify(rest))
+			setAccessToken(accessToken)
+			setRefreshToken(refreshToken)
+			setUser(JSON.stringify(rest))
 		})
 }
 
 const logout = () => {
-	// remove access refresh tokens from local storage to log user out
-	localStorage.removeItem('accessToken')
-	localStorage.removeItem('refreshToken')
-	localStorage.removeItem('user')
+	removeUserAndTokens()
 }
 
 const getAll = () => {
 	const requestOptions = {
-		method: 'GET',
-		headers: authHeader()
+		method: 'GET'
 	}
 
 	return fetch(`${config.api.url}/users`, requestOptions)
@@ -81,6 +116,6 @@ export const usersService = {
 	register,
 	login,
 	logout,
-	getTokenPair,
+	refreshTokenPair,
 	getAll
 }
